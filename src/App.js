51046d9053fce82b2bf27e4b0115265c2958da90
Sergey Markov <pro-markov@yandex.ru>
2021-03-05 10:59:05 +0300

    Добавляет 5 урок

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 import { Header } from './components';
 import { Home, Cart } from './pages';
 import { Route } from 'react-router-dom';
 
 function App() {
+
+const [pizzas, setPizzas] = useState([]);
+
+useEffect(() => {
+  axios('http://localhost:3000/db.json')
+  .then(({data}) => setPizzas(data.pizzas))
+}, [])
+
+
   return (
     <div className="wrapper">
 
       <Header/>
 
       <div className="content">
-        <Route path="/" component={Home} exact />
+        <Route path="/" exact >
+          <Home items={pizzas}/>
+        </Route>
         <Route path="/cart" component={Cart} exact />
       </div>
-
     </div>
   );
 }
